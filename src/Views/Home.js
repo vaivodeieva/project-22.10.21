59e4074a257d50da6315b2735a209ae654ffbeb2
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-10-25 21:41:34 +0300

    slider hover effect

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,12 +1,85 @@
-
+import '../Assets/Css/Home.css';
 
 function Home() {
 
     return (
         <div>
 
-        </div>
-    )
+            <h2 class="text-center">Location Slider - Hover to see effect</h2>
+            <img src="https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" width="100%" height="450px" />
+            <div class="slider-info-wrap clearfix pdlr0">
+                <div class="slider-info-content">
+
+                    <div class="slider-info-item">
+                        <div class="info-item-media"><img src="https://images.unsplash.com/photo-1503220317375-aaad61436b1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-3@2x.jpg" alt="" />
+                            <div class="info-item-text">
+                                <div class="info-price font-4"><span></span></div>
+                                <div class="info-temp font-4"><span>local temperature</span> 41° / 105.8°</div>
+                                <p class="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </p>
+                            </div>
+                        </div>
+                        <div class="info-item-content">
+                            <div class="main-title">
+                                <h3 class="title"><span class="font-4"></span>Location</h3>
+                                <div class="price"><span>2 Night 7 Days</span></div>
+                                <span class="button">2 Night 7 Days</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="slider-info-item">
+                        <div class="info-item-media"><img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" />
+                            <div class="info-item-text">
+                                <div class="info-price font-4"><span></span></div>
+                                <div class="info-temp font-4"><span>local temperature</span> 31° / 87.8°</div>
+                                <p class="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </p>
+                            </div>
+                        </div>
+                        <div class="info-item-content">
+                            <div class="main-title">
+                                <h3 class="title"><span class="font-4"></span> London</h3>
+                                <div class="price"><span>2 Night 7 Days</span></div>
+                                <span class="button">2 Night 7 Days</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slider-info-item">
+                        <div class="info-item-media"><img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" />
+                            <div class="info-item-text">
+                                <div class="info-price font-4"><span></span></div>
+                                <div class="info-temp font-4"><span>local temperature</span> 31° / 87.8°</div>
+                                <p class="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </p>
+                            </div>
+                        </div>
+                        <div class="info-item-content">
+                            <div class="main-title">
+                                <h3 class="title"><span class="font-4"></span> India</h3>
+                                <div class="price"><span>2 Night 7 Days</span></div>
+                                <span class="button">2 Night 7 Days</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slider-info-item">
+                        <div class="info-item-media"><img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" />
+                            <div class="info-item-text">
+                                <div class="info-price font-4"><span></span></div>
+                                <div class="info-temp font-4"><span>local temperature</span> 31° / 87.8°</div>
+                                <p class="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </p>
+                            </div>
+                        </div>
+                        <div class="info-item-content">
+                            <div class="main-title">
+                                <h3 class="title"><span class="font-4"></span> Singapore</h3>
+                                <div class="price"><span>2 Night 7 Days</span></div>
+                                <span class="button">2 Night 7 Days</span>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+
+                </div>
+                )
 }
 
-export default Home;
+                export default Home;
