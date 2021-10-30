d1cc62fc2b2def6f740453ac3da611bc796e4095
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-10-30 21:23:48 +0300

    update page404

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -5,15 +5,15 @@ function Page404() {
     return (
 
         <div>
-            <h1>404 Error Page #3</h1>
-            <p class="zoom-area"><b>CSS</b> animations to make a cool 404 page. </p>
-            <section class="error-container">
+            <h1>404 Error Page</h1>
+            <p className="zoom-area"><b>CSS</b> animations to make a cool 404 page. </p>
+            <section className="error-container">
                 <span>4</span>
-                <span><span class="screen-reader-text">0</span></span>
+                <span><span className="screen-reader-text">0</span></span>
                 <span>4</span>
             </section>
-            <div class="link-container">
-                <a target="_blank" href="https://www.silocreativo.com/en/creative-examples-404-error-css/" class="more-link">Visit the original article</a>
+            <div className="link-container">
+                <a target="_blank" href="https://www.silocreativo.com/en/creative-examples-404-error-css/" className="more-link">Visit the original article</a>
             </div>
 
         </div>
