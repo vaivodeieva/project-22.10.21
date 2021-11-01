03d5bfa050d8f517316061653d74b9b77f0117f4
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-11-01 19:40:51 +0200

    update footer, add link to head

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,6 +4,19 @@ function Footer() {
 
         <div>
 
+  <footer class="rodape" id="rodape">
+    <ul class="redes">
+      <li data-text="Facebook"><a target="blanck" class="facebook" href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+      <li data-text="Instagram"><a target="blanck" class="instagram" href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+      <li data-text="Twitter"><a target="blanck" class="twitter" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+      <li data-text="Youtube"><a target="blanck" class="youtube" href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+      <li data-text="Linkedin"><a target="blanck" class="linkedin" href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+      <li data-text="Githube"><a target="blanck" class="githube" href="#"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+      <li data-text="Codepen"><a target="blanck" class="codepen" href="#"><i class="fa fa-codepen" aria-hidden="true"></i></a></li>
+    </ul>
+  </footer>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/vanilla-tilt/1.7.0/vanilla-tilt.min.js"></script>
+
         </div>
     )
 }
