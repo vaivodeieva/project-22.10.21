7fc16d558120864097ca21b863918644d19c54e4
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-11-03 12:42:54 +0200

    update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 import '../Assets/Css/Footer.css';
 // import VanillaTilt from 'vanilla-tilt';
-import React from 'react';
-import Tilt from 'react-vanilla-tilt';
+// import React from 'react';
+import Tilt from 'react-vanilla-tilt'; 
 
 // VanillaTilt.init(document.querySelectorAll(".redes li a"), {
 //   max: 30,
@@ -12,16 +12,14 @@ import Tilt from 'react-vanilla-tilt';
 
 function Footer() {
 
-
   // Source Code for Vanilla Javascript Tilt: https://cdnjs.cloudflare.com/ajax/libs/vanilla-tilt/1.7.0/vanilla-tilt.min.js
 
-  // data-tilt data-tilt-max="50" data-tilt-speed="400" data-tilt-perspective="500"
   return (
 
     <div>
       <footer className="rodape" id="rodape">
         <ul className="redes">
-          <li data-text="Facebook"> <Tilt options={{ max: 30, speed: 400, glare: true, "max-glare": 1 }}><a target="blanck" className="facebook" href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></Tilt></li>
+        <li data-text="Facebook"><Tilt options={{ max: 50, speed: 400, glare: true, "max-glare": 1 }}><a target="blanck" className="facebook" href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></Tilt></li>
           <li data-text="Instagram"><a target="blanck" className="instagram" href="#"><i className="fa fa-instagram" aria-hidden="true" data-tilt></i></a></li>
           <li data-text="Twitter"><a target="blanck" className="twitter" href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
           <li data-text="Youtube"><a target="blanck" className="youtube" href="#"><i className="fa fa-youtube" aria-hidden="true"></i></a></li>
