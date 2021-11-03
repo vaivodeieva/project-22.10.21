05397cfd549cdf9882e22d1026802d151c298dd1
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-11-03 11:53:51 +0200

    adding react-vanilla-tilt

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,14 @@
 import '../Assets/Css/Footer.css';
-import VanillaTilt from 'vanilla-tilt';
+// import VanillaTilt from 'vanilla-tilt';
+import React from 'react';
+import Tilt from 'react-vanilla-tilt';
 
-VanillaTilt.init(document.querySelectorAll(".redes li a"), {
-  max: 30,
-  speed: 400,
-  glare: true,
-  "max-glare": 1
-});
+// VanillaTilt.init(document.querySelectorAll(".redes li a"), {
+//   max: 30,
+//   speed: 400,
+//   glare: true,
+//   "max-glare": 1
+// });
 
 function Footer() {
 
@@ -17,19 +19,17 @@ function Footer() {
   return (
 
     <div>
-
-      <footer className="rodape" id="rodape">
-        <ul className="redes" >
-          <li data-text="Facebook"><a target="blanck" className="facebook" href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-          <li data-text="Instagram"><a target="blanck" className="instagram" href="#"><i className="fa fa-instagram" aria-hidden="true" data-tilt></i></a></li>
-          <li data-text="Twitter"><a target="blanck" className="twitter" href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-          <li data-text="Youtube"><a target="blanck" className="youtube" href="#"><i className="fa fa-youtube" aria-hidden="true"></i></a></li>
-          <li data-text="Linkedin"><a target="blanck" className="linkedin" href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-          <li data-text="Githube"><a target="blanck" className="githube" href="#"><i className="fa fa-github" aria-hidden="true"></i></a></li>
-          <li data-text="Codepen"><a target="blanck" className="codepen" href="#"><i className="fa fa-codepen" aria-hidden="true"></i></a></li>
-        </ul>
-      </footer>
-
+        <footer className="rodape" id="rodape">
+          <ul className="redes">
+          <Tilt><li data-text="Facebook"><a target="blanck" className="facebook" href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li></Tilt>
+            <li data-text="Instagram"><a target="blanck" className="instagram" href="#"><i className="fa fa-instagram" aria-hidden="true" data-tilt></i></a></li>
+            <li data-text="Twitter"><a target="blanck" className="twitter" href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li data-text="Youtube"><a target="blanck" className="youtube" href="#"><i className="fa fa-youtube" aria-hidden="true"></i></a></li>
+            <li data-text="Linkedin"><a target="blanck" className="linkedin" href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            <li data-text="Githube"><a target="blanck" className="githube" href="#"><i className="fa fa-github" aria-hidden="true"></i></a></li>
+            <li data-text="Codepen"><a target="blanck" className="codepen" href="#"><i className="fa fa-codepen" aria-hidden="true"></i></a></li>
+          </ul>
+        </footer>
     </div>
   )
 }
