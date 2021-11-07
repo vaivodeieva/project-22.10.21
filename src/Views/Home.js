9a0d745bc3e02c124cf0a9fddec1c57126763633
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-11-07 23:43:12 +0200

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -21,8 +21,8 @@ function Home() {
                         <div class="info-item-content">
                             <div class="main-title">
                                 <h3 class="title"><span class="font-4"></span>Location</h3>
-                                <div class="price"><span>2 Night 7 Days</span></div>
-                                <span class="button">2 Night 7 Days</span>
+                                <div class="price"><span>Lorem Ipsum</span></div>
+                                <span class="button">some text</span>
                             </div>
                         </div>
                     </div>
@@ -37,9 +37,25 @@ function Home() {
                         </div>
                         <div class="info-item-content">
                             <div class="main-title">
-                                <h3 class="title"><span class="font-4"></span> London</h3>
-                                <div class="price"><span>2 Night 7 Days</span></div>
-                                <span class="button">2 Night 7 Days</span>
+                                <h3 class="title"><span class="font-4"></span> Option 2</h3>
+                                <div class="price"><span>Lorem Ipsum</span></div>
+                                <span class="button">some text</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slider-info-item">
+                        <div class="info-item-media"><img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" />
+                            <div class="info-item-text">
+                                <div class="info-price font-4"><span></span></div>
+                                <div class="info-temp font-4"><span>local temperature</span> Option 2</div>
+                                <p class="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </p>
+                            </div>
+                        </div>
+                        <div class="info-item-content">
+                            <div class="main-title">
+                                <h3 class="title"><span class="font-4"></span> Option 3</h3>
+                                <div class="price"><span>Lorem Ipsum</span></div>
+                                <span class="button">some text</span>
                             </div>
                         </div>
                     </div>
@@ -53,25 +69,9 @@ function Home() {
                         </div>
                         <div class="info-item-content">
                             <div class="main-title">
-                                <h3 class="title"><span class="font-4"></span> India</h3>
-                                <div class="price"><span>2 Night 7 Days</span></div>
-                                <span class="button">2 Night 7 Days</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="slider-info-item">
-                        <div class="info-item-media"><img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" />
-                            <div class="info-item-text">
-                                <div class="info-price font-4"><span></span></div>
-                                <div class="info-temp font-4"><span>local temperature</span> 31° / 87.8°</div>
-                                <p class="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </p>
-                            </div>
-                        </div>
-                        <div class="info-item-content">
-                            <div class="main-title">
-                                <h3 class="title"><span class="font-4"></span> Singapore</h3>
-                                <div class="price"><span>2 Night 7 Days</span></div>
-                                <span class="button">2 Night 7 Days</span>
+                                <h3 class="title"><span class="font-4"></span> Option 4</h3>
+                                <div class="price"><span>Lorem Ipsum</span></div>
+                                <span class="button">some text</span>
                             </div>
                         </div>
                     </div>
