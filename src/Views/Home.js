f2b02cbaf92160a93d0644cb586dd2d10f77fbe1
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-11-08 09:51:23 +0200

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -14,13 +14,13 @@ function Home() {
                         <div class="info-item-media"><img src="https://images.unsplash.com/photo-1503220317375-aaad61436b1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-3@2x.jpg" alt="" />
                             <div class="info-item-text">
                                 <div class="info-price font-4"><span></span></div>
-                                <div class="info-temp font-4"><span>local temperature</span> 41° / 105.8°</div>
+                                <div class="info-temp font-4"><span>local temperature</span> some text</div>
                                 <p class="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
                             </div>
                         </div>
                         <div class="info-item-content">
                             <div class="main-title">
-                                <h3 class="title"><span class="font-4"></span>Location</h3>
+                                <h3 class="title"><span class="font-4"></span>Option 1</h3>
                                 <div class="price"><span>Lorem Ipsum</span></div>
                                 <span class="button">some text</span>
                             </div>
@@ -31,7 +31,7 @@ function Home() {
                         <div class="info-item-media"><img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" />
                             <div class="info-item-text">
                                 <div class="info-price font-4"><span></span></div>
-                                <div class="info-temp font-4"><span>local temperature</span> 31° / 87.8°</div>
+                                <div class="info-temp font-4"><span>local temperature</span> some text</div>
                                 <p class="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text </p>
                             </div>
                         </div>
@@ -47,7 +47,7 @@ function Home() {
                         <div class="info-item-media"><img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" />
                             <div class="info-item-text">
                                 <div class="info-price font-4"><span></span></div>
-                                <div class="info-temp font-4"><span>local temperature</span> Option 2</div>
+                                <div class="info-temp font-4"><span>local temperature</span> some text</div>
                                 <p class="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
                             </div>
                         </div>
@@ -63,7 +63,7 @@ function Home() {
                         <div class="info-item-media"><img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" />
                             <div class="info-item-text">
                                 <div class="info-price font-4"><span></span></div>
-                                <div class="info-temp font-4"><span>local temperature</span> 31° / 87.8°</div>
+                                <div class="info-temp font-4"><span>local temperature</span> some text</div>
                                 <p class="info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
                             </div>
                         </div>
