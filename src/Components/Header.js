cae28f6455b9bc7e64cebb60368db3fa9fa9822e
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-11-09 15:52:47 +0200

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,9 +10,9 @@ function Header() {
                     <h1>E-STORE</h1>
                     <nav>
                         <a href="/">Home</a>
-                        <a href="#">Categories</a>
-                        <a href="#">Cart</a>
-                        <a href="#">About us</a>
+                        <a href="/categories">Categories</a>
+                        <a href="/cart">Cart</a>
+                        <a href="/aboutus">About us</a>
                     </nav>
                 </header>
             </main>
