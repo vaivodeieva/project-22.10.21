a4c2436ab7153804827a896f5252834f43ee291a
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-11-10 13:31:52 +0200

    update home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,5 +1,9 @@
 import '../Assets/Css/Home.css';
 import homepicture from '../Assets/Images/home-pic.png';
+import pic1 from '../Assets/Images/pic1.jpg';
+import pic2 from '../Assets/Images/pic2.jpg';
+import pic3 from '../Assets/Images/pic3.jpeg';
+import pic4 from '../Assets/Images/pic4.jpg';
 
 function Home() {
 
@@ -12,7 +16,9 @@ function Home() {
                 <div className="slider-info-content">
 
                     <div className="slider-info-item">
-                        <div className="info-item-media"><img src="https://images.unsplash.com/photo-1503220317375-aaad61436b1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-3@2x.jpg" alt="" />
+                        <div className="info-item-media">
+                            {/* <img src="https://images.unsplash.com/photo-1503220317375-aaad61436b1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-3@2x.jpg" alt="" /> */}
+                            <img src={pic1}/>
                             <div className="info-item-text">
                                 <div className="info-price font-4"><span></span></div>
                                 <div className="info-temp font-4"><span>local temperature</span> some text</div>
@@ -29,7 +35,9 @@ function Home() {
                     </div>
 
                     <div className="slider-info-item">
-                        <div className="info-item-media"><img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" />
+                        <div className="info-item-media">
+                            {/* <img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" /> */}
+                            <img src={pic2}/>
                             <div className="info-item-text">
                                 <div className="info-price font-4"><span></span></div>
                                 <div className="info-temp font-4"><span>local temperature</span> some text</div>
@@ -45,7 +53,9 @@ function Home() {
                         </div>
                     </div>
                     <div className="slider-info-item">
-                        <div className="info-item-media"><img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" />
+                        <div className="info-item-media">
+                            {/* <img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" /> */}
+                            <img src={pic3}/>
                             <div className="info-item-text">
                                 <div className="info-price font-4"><span></span></div>
                                 <div className="info-temp font-4"><span>local temperature</span> some text</div>
@@ -61,7 +71,9 @@ function Home() {
                         </div>
                     </div>
                     <div className="slider-info-item">
-                        <div className="info-item-media"><img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" />
+                        <div className="info-item-media">
+                            {/* <img src="https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" data-at2x="pic/slider-info-2@2x.jpg" alt="" /> */}
+                            <img src={pic4}/>
                             <div className="info-item-text">
                                 <div className="info-price font-4"><span></span></div>
                                 <div className="info-temp font-4"><span>local temperature</span> some text</div>
