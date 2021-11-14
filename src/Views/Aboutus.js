6081860f974ce236ea7ace4d7a46d7e2aeb3f67c
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-11-14 21:33:24 +0200

    Update Aboutus.js

--- src/Views/Aboutus.js
+++ src/Views/Aboutus.js
@@ -5,27 +5,7 @@ function Aboutus() {
 
         <div>
 
-            <ul className="nav nav-pills">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Active</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                        <li><hr className="dropdown-divider" /></li>
-                        <li><a className="dropdown-item" href="#">Separated link</a></li>
-                    </ul>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
+            
 
         </div>
     )
