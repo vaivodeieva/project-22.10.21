525be31936bdcdb18b0344ee4e3625a2f2811c18
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-11-19 13:33:03 +0200

    Update Cart.js

--- src/Views/Cart.js
+++ src/Views/Cart.js
@@ -11,14 +11,9 @@ let item;
 let itemCopy;
 let i;
 
-    // ************************************************
-// Shopping Cart API
-// ************************************************
-
+   
 let shoppingCart = (function() {
-    // =============================
-    // Private methods and propeties
-    // =============================
+    
     cart = [];
     
     // Constructor
@@ -42,9 +37,6 @@ let shoppingCart = (function() {
     }
     
   
-    // =============================
-    // Public methods and propeties
-    // =============================
     let obj = {};
     
     // Add to cart
@@ -175,10 +167,10 @@ let shoppingCart = (function() {
       output += "<tr>"
         + "<td>" + cartArray[i].name + "</td>" 
         + "<td>(" + cartArray[i].price + ")</td>"
-        + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
-        + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
-        + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
-        + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+        + "<td><div className='input-group'><button className='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
+        + "<input type='number' className='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+        + "<button className='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
+        + "<td><button className='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
         + " = " 
         + "<td>" + cartArray[i].total + "</td>" 
         +  "</tr>";
@@ -225,68 +217,68 @@ let shoppingCart = (function() {
 
         <div>
             {/* <!-- Nav --> */}
-            <nav class="navbar navbar-inverse bg-inverse fixed-top bg-faded">
-                <div class="row">
-                    <div class="col">
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#cart">Cart (<span class="total-count"></span>)</button><button class="clear-cart btn btn-danger">Clear Cart</button>
+            <nav className="navbar navbar-inverse bg-inverse fixed-top bg-faded">
+                <div className="row">
+                    <div className="col">
+                        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#cart">Cart (<span className="total-count"></span>)</button><button className="clear-cart btn btn-danger">Clear Cart</button>
                     </div>
                 </div>
             </nav>
 
             {/* <!-- Main --> */}
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <div class="card" style={{width: '20rem'}}>
-                            <img class="card-img-top" src="http://www.azspagirls.com/files/2010/09/orange.jpg" alt="Card image cap"/>
-                            <div class ="card-block">
-                            <h4 class ="card-title">Orange</h4>
-                            <p class ="card-text">Price: $0.5</p>
-                            <a href="#" data-name="Orange" data-price="0.5" class ="add-to-cart btn btn-primary">Add to cart</a>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div className="card" style={{width: '20rem'}}>
+                            <img className="card-img-top" src="http://www.azspagirls.com/files/2010/09/orange.jpg" alt="Card image cap"/>
+                            <div className ="card-block">
+                            <h4 className ="card-title">Orange</h4>
+                            <p className ="card-text">Price: $0.5</p>
+                            <a href="#" data-name="Orange" data-price="0.5" className ="add-to-cart btn btn-primary">Add to cart</a>
                             </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card" style={{width: '20rem'}}>
-                            <img class="card-img-top" src="http://images.all-free-download.com/images/graphicthumb/vector_illustration_of_ripe_bananas_567893.jpg" alt="Card image cap"/>
-                            <div class ="card-block">
-                            <h4 class ="card-title">Banana</h4>
-                            <p class ="card-text">Price: $1.22</p>
-                            <a href="#" data-name="Banana" data-price="1.22" class ="add-to-cart btn btn-primary">Add to cart</a>
+                    <div className="col">
+                        <div className="card" style={{width: '20rem'}}>
+                            <img className="card-img-top" src="http://images.all-free-download.com/images/graphicthumb/vector_illustration_of_ripe_bananas_567893.jpg" alt="Card image cap"/>
+                            <div className ="card-block">
+                            <h4 className ="card-title">Banana</h4>
+                            <p className ="card-text">Price: $1.22</p>
+                            <a href="#" data-name="Banana" data-price="1.22" className ="add-to-cart btn btn-primary">Add to cart</a>
                             </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card" style={{width: '20rem'}}>
-                            <img class="card-img-top" src="https://3.imimg.com/data3/IC/JO/MY-9839190/organic-lemon-250x250.jpg" alt="Card image cap"/>
-                            <div class ="card-block">
-                            <h4 class ="card-title">Lemon</h4>
-                            <p class ="card-text">Price: $5</p>
-                            <a href="#" data-name="Lemon" data-price="5" class ="add-to-cart btn btn-primary">Add to cart</a>
+                    <div className="col">
+                        <div className="card" style={{width: '20rem'}}>
+                            <img className="card-img-top" src="https://3.imimg.com/data3/IC/JO/MY-9839190/organic-lemon-250x250.jpg" alt="Card image cap"/>
+                            <div className ="card-block">
+                            <h4 className ="card-title">Lemon</h4>
+                            <p className ="card-text">Price: $5</p>
+                            <a href="#" data-name="Lemon" data-price="5" className ="add-to-cart btn btn-primary">Add to cart</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="modal fade" id="cart" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Cart</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <div className="modal fade" id="cart" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-lg" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Cart</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
-                        <div class="modal-body">
-                            <table class="show-cart table">
+                        <div className="modal-body">
+                            <table className="show-cart table">
 
                             </table>
-                            <div>Total price: $<span class="total-cart"></span></div>
+                            <div>Total price: $<span className="total-cart"></span></div>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Order now</button>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Order now</button>
                         </div>
                     </div>
                 </div>
