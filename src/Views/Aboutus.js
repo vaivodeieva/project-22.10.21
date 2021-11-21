8aacd7dd9939747979635c91c9d80604d5dca7d0
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-11-21 10:28:49 +0200

    Update Aboutus.js

--- src/Views/Aboutus.js
+++ src/Views/Aboutus.js
@@ -5,7 +5,22 @@ function Aboutus() {
 
         <div>
 
-            
+            <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Active</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+
+
 
         </div>
     )
