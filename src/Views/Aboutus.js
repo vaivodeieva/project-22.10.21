683d050c58e071313b91b99aa9105d9fa91a5333
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-11-24 11:18:47 +0200

    Update Aboutus.js

--- src/Views/Aboutus.js
+++ src/Views/Aboutus.js
@@ -103,6 +103,32 @@ function Aboutus() {
                     </div>
                     <div className="col-md-6">
                         <h3>How to find us</h3>
+                        <div className="row map-and-address">
+                            <div className="col map">
+                                <div className="mapouter">
+                                    <div className="gmap_canvas"><iframe className="gmap_iframe" frameborder="0" scrolling="no" marginheight="0"
+                                        marginwidth="0"
+                                        src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=The Courtyard, Edenhall, Penrith CA11 8ST, United Kingdom&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a
+                                            href="https://www.fridaynightfunkin.net/"></a></div>
+
+                                </div>
+                            </div>
+                            <div className="col address">
+                                <h4>Address:</h4>
+
+                                <p>
+                                    Edenhall Country, Oxford OX33 1EA, United Kingdom
+                                </p>
+                                <h4>EMail:</h4>
+                                <p>
+                                    info@example.com
+                                </p>
+                                <h4>Phone:</h4>
+                                <p>
+                                    +44 XXXXXXXXXXX
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
